d806812837f9fdf4d1c3a8e889e3ed6d66545485
mtorcellini <matt.torcellini@gmail.com>
2020-11-10 14:28:53 -0500

    apiroutes file setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.port || 8080;
 const htmlroutes = require('./routes/htmlroutes.js');
+const apiroutes = require('./routes/apiroutes.js');
 
 htmlroutes(app);
 
