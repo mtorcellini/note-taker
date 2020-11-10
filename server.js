4e0b6f867aa4622b7b5ea10bf9f5168109774209
mtorcellini <matt.torcellini@gmail.com>
2020-11-10 17:34:38 -0500

    change port to 3001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.port || 3001;
 const htmlroutes = require('./routes/htmlroutes.js');
 const apiroutes = require('./routes/apiroutes.js');
 
