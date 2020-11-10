e03d51afa7269d6916d9c99bd19c30d88273353f
mtorcellini <matt.torcellini@gmail.com>
2020-11-10 13:19:21 -0500

    notes route

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.port || 8080;
 
+app.use(express.static('public'));
+
+app.get('/notes', (req, res, next) => {
+    console.log('Sending file ' + __dirname + '/public/notes.html');
+    res.sendFile(__dirname + '/public/notes.html');
+});
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
