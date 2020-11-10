baed384c8de0e9e0f783b84191152922c880dd2d
mtorcellini <matt.torcellini@gmail.com>
2020-11-10 17:37:07 -0500

    correct port to PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const htmlroutes = require('./routes/htmlroutes.js');
 const apiroutes = require('./routes/apiroutes.js');
 
